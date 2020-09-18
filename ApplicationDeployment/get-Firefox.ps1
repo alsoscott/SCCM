ac6380c5178ada63bb3106579437ba44c71e73a3
Scott Churchill <alsoscott@users.noreply.github.com>
2020-09-17 22:49:10 -0700

    massive WIP, re-write coming soon

--- ApplicationDeployment/get-Firefox.ps1
+++ ApplicationDeployment/get-Firefox.ps1
@@ -23,10 +23,12 @@ Required additional script needed: Get-MSIFileInformation.ps1 which can be obtai
 param ([switch]$FirstRun)
  
 
+
+#Move to site config - $Script:FinalDirectory = "\\server\share" #final destination for Firefox installers
+#Move to site config - $Script:SiteCode = "AZR" # SCCM Site code 
+#Move to site config - $Script:ProviderMachineName = "SCCM Site Server" # SMS Provider machine name
+
 $Script:TempDirectory = $env:TEMP #inital download directory
-$Script:FinalDirectory = "\\server\share" #final destination for Firefox installers
-$Script:SiteCode = "AZR" # SCCM Site code 
-$Script:ProviderMachineName = "SCCM Site Server" # SMS Provider machine name
 $Script:CurrentPkgName = "Mozilla Firefox - Current"
 $Script:FirefoxVersion = $null
 $Script:FirefoxColl = "Deploy | Mozilla Firefox"
@@ -99,8 +101,12 @@ Function UpdateFiles {
 
 #update deployment package on SCCM to current version, and redistribute
 Function SetSCCMDeployment {
+    param ([switch]$FirstRun)
+    $curLoc = (Get-Location)
 Write-Host "Connecting to SCCM Site Server" -ForegroundColor Yellow
 #site connection
+#update based on site config - this should be a one-liner
+<#
 $initParams = @{}
 if((Get-Module ConfigurationManager) -eq $null) {
     Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
@@ -109,30 +115,49 @@ if((Get-PSDrive -Name $Script:SiteCode -PSProvider CMSite -ErrorAction SilentlyC
     New-PSDrive -Name $Script:SiteCode -PSProvider CMSite -Root $Script:ProviderMachineName @initParams
 }
 Set-Location "$($Script:SiteCode):\" @initParams
+#>
+
 
 #Update Collection membership rules for Outdated Chrome
 $query = "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_INSTALLED_SOFTWARE on SMS_G_System_INSTALLED_SOFTWARE.ResourceID = SMS_R_System.ResourceId where SMS_G_System_INSTALLED_SOFTWARE.ProductName like 'Mozilla Firefox%' and SMS_G_System_INSTALLED_SOFTWARE.ProductVersion < '$Script:FirefoxVersion'"
-remove-CMDeviceCollectionQueryMembershipRule -CollectionName $Script:FirefoxOutDateColl -RuleName "Firefox Outdated" -Force -ErrorAction SilentlyContinue
-add-CMDeviceCollectionQueryMembershipRule -CollectionName $Script:FirefoxOutDateColl -RuleName "Firefox Outdated" -QueryExpression $query -Force
+if ($FirstRun){
+    #create collections - Query and Deploy
+    #create query membership
+    #include query in deployment
+    #create folders - use if (AppQuery \ AppDeploy)
+    #move to folders
+    
+} else {
+    remove-CMDeviceCollectionQueryMembershipRule -CollectionName $Script:FirefoxOutDateColl -RuleName "Firefox Outdated" -Force -ErrorAction SilentlyContinue
+    add-CMDeviceCollectionQueryMembershipRule -CollectionName $Script:FirefoxOutDateColl -RuleName "Firefox Outdated" -QueryExpression $query -Force
+    
+}
 
 #Build updated Detection Method Clauses
 $clause1 = New-CMDetectionClauseFile -Value -Path "%ProgramFiles%\Mozilla Firefox\" -FileName "Firefox.exe" -PropertyType Version -ExpressionOperator GreaterEquals -ExpectedValue $Script:FirefoxVersion
-$clause2 = New-CMDetectionClauseFile -Value -Path "%ProgramFiles(x86)%\Mozilla Firefox\" -FileName "firefox.exe" -PropertyType Version -ExpressionOperator GreaterEquals -ExpectedValue $Script:FirefoxVersion 
-$clause1.Connector = 'Or'
-$clause2.Connector = 'Or'
 #pulling Package information to update
-$SDMPackageXML = (Get-CMDeploymentType -ApplicationName "$($Script:CurrentPkgName)" -DeploymentTypeName "$($Script:CurrentPkgName)").SDMPackageXML
-[string[]]$OldDetections = (([regex]'(?<=SettingLogicalName=.)([^"]|\\")*').Matches($SDMPackageXML)).Value
+
+if (!($FirstRun)){
+    $SDMPackageXML = (Get-CMDeploymentType -ApplicationName "$($Script:CurrentPkgName)" -DeploymentTypeName "$($Script:CurrentPkgName)").SDMPackageXML
+    [string[]]$OldDetections = (([regex]'(?<=SettingLogicalName=.)([^"]|\\")*').Matches($SDMPackageXML)).Value
+} 
 #paramaters set, populatating chagnes in SCCM now
-Write-Host "Updating Deployment Packages" -ForegroundColor Yellow
-Set-CMApplication -Name $Script:CurrentPkgName -SoftwareVersion $Script:FirefoxVersion
-Set-CMScriptDeploymentType -ApplicationName $Script:CurrentPkgName -DeploymentTypeName $Script:CurrentPkgName -ContentLocation $Script:FinalDirectory\$Script:FirefoxVersion -RemoveDetectionClause $OldDetections -AddDetectionClause($clause1,$clause2)
-#Set-CMScriptDeploymentType -ApplicationName $PreviousPkgName -DeploymentTypeName $PreviousPkgName -ContentLocation $PreviousLocation
-Write-Host "Redistributing Content" -ForegroundColor Yellow
-Update-CMDistributionPoint -ApplicationName $Script:CurrentPkgName -DeploymentTypeName $Script:CurrentPkgName
-Set-CMApplicationDeployment -ApplicationName $Script:CurrentPkgName -CollectionName $Script:FirefoxColl -AvailableDateTime (get-date 06:00:00).AddDays(0) -DeadlineDateTime (get-date 18:00:00).AddDays(1)
+if ($FirstRun){
+    new app
+    new deployment type
+    distrubute content
+    new deployment
+} else {
+    Write-Host "Updating Deployment Packages" -ForegroundColor Yellow
+    Set-CMApplication -Name $Script:CurrentPkgName -SoftwareVersion $Script:FirefoxVersion
+    Set-CMScriptDeploymentType -ApplicationName $Script:CurrentPkgName -DeploymentTypeName $Script:CurrentPkgName -ContentLocation $Script:FinalDirectory\$Script:FirefoxVersion -RemoveDetectionClause $OldDetections -AddDetectionClause($clause1)
+    Write-Host "Redistributing Content" -ForegroundColor Yellow
+    Update-CMDistributionPoint -ApplicationName $Script:CurrentPkgName -DeploymentTypeName $Script:CurrentPkgName
+    Set-CMApplicationDeployment -ApplicationName $Script:CurrentPkgName -CollectionName $Script:FirefoxColl -AvailableDateTime (get-date 06:00:00).AddDays(0) -DeadlineDateTime (get-date 18:00:00).AddDays(1)
+}
+
 #return to local disk
-sl c:
+Set-Location $curLoc
 }
 
 Function CreateBatch {
@@ -142,21 +167,13 @@ $installbatch=@'
 @echo off
 taskkill /IM firefox.exe /f 2> nul
 :CheckOS
-IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
+IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO END)
 :64BIT
 "%~dp0MozillaFirefox64.exe" -ms
 if exist "%programfiles%\Mozilla Firefox\" copy /Y "%~dp0override.ini" "%programfiles%\Mozilla Firefox\browser\"
 if exist "%programfiles%\Mozilla Firefox\" copy /Y "%~dp0mozilla.cfg" "%programfiles%\Mozilla Firefox\"
 if exist "%programfiles%\Mozilla Firefox\" copy /Y "%~dp0local-settings.js" "%programfiles%\Mozilla Firefox\defaults\pref"
 if exist "%programfiles%\Mozilla Firefox\" copy /Y "%~dp0trustwubcerts.js" "%programfiles%\Mozilla Firefox\defaults\pref"
-GOTO END
-
-:32BIT
-"%~dp0MozillaFirefox32.exe" -ms
-if exist "%ProgramFiles(x86)%\Mozilla Firefox\" copy /Y "%~dp0override.ini" "%ProgramFiles(x86)%\Mozilla Firefox\browser\"
-if exist "%ProgramFiles(x86)%\Mozilla Firefox\" copy /Y "%~dp0mozilla.cfg" "%ProgramFiles(x86)%\Mozilla Firefox\"
-if exist "%ProgramFiles(x86)%\Mozilla Firefox\" copy /Y "%~dp0local-settings.js" "%ProgramFiles(x86)%\Mozilla Firefox\defaults\pref"
-if exist "%ProgramFiles(x86)%\Mozilla Firefox\" copy /Y "%~dp0trustwubcerts.js" "%ProgramFiles(x86)%\Mozilla Firefox\defaults\pref"
 GOTO END
 
 :END
@@ -182,94 +199,16 @@ GOTO END
 exit /B %EXIT_CODE%
 '@
 
-
-$localsettings_js=@'
-pref("general.config.obscure_value", 0);
-pref("general.config.filename", "mozilla.cfg");
-'@
-
-$mozilla_cfg=@'
-//Firefox Default Settings
-// set Firefox Default homepage
-pref("browser.startup.homepage","https://www.azasrs.gov");
-// disable default browser check
-pref("browser.shell.checkDefaultBrowser", false);
-pref("browser.startup.homepage_override.mstone", "ignore");
-// disable application updates
-pref("app.update.auto", false)
-pref("app.update.enabled", false)
-// disables the 'know your rights' button from displaying on first run
-pref("browser.rights.3.shown", true);
-// disables the request to send performance data from displaying
-pref("toolkit.telemetry.prompted", 2);
-pref("toolkit.telemetry.rejected", true);
-'@
-
-$override_ini=@'
-[XRE]
-EnableProfileMigrator=false
-'@
-
-$trustwebcerts_js=@'
-pref("security.enterprise_roots.enabled", true);
-'@
-
-
 $installbatch | out-file $Script:TempDirectory\$Script:FirefoxVersion\install.bat -Encoding ascii
 $uninstallbatch | out-file $Script:TempDirectory\$Script:FirefoxVersion\uninstall.bat -Encoding ascii
-$localsettings_js | out-file $Script:TempDirectory\$Script:FirefoxVersion\local-settings.js -Encoding ascii
-$mozilla_cfg | out-file $Script:TempDirectory\$Script:FirefoxVersion\mozilla.cfg -Encoding ascii
-$override_ini | out-file $Script:TempDirectory\$Script:FirefoxVersion\override.ini -Encoding ascii
-$trustwebcerts_js | out-file $Script:TempDirectory\$Script:FirefoxVersion\trustwubcerts.js -Encoding ascii
-
 }
 
 
 
-Function FirstRun {
-    Write-Host "Connecting to SCCM Site Server" -ForegroundColor Yellow
-    #site connection
-    $initParams = @{}
-    if((Get-Module ConfigurationManager) -eq $null) {
-        Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
-    }
-    if((Get-PSDrive -Name $Script:SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
-        New-PSDrive -Name $Script:SiteCode -PSProvider CMSite -Root $Script:ProviderMachineName @initParams
-    }
-    Set-Location "$($Script:SiteCode):\" @initParams
-    
-    #New Collections - Outdated Query based collection, and Deployment collection limited to Outdated Collection
-    $query = "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_INSTALLED_SOFTWARE on SMS_G_System_INSTALLED_SOFTWARE.ResourceID = SMS_R_System.ResourceId where SMS_G_System_INSTALLED_SOFTWARE.ProductName like 'Mozilla Firefox%' and SMS_G_System_INSTALLED_SOFTWARE.ProductVersion < '$Script:FirefoxVersion'"
-    New-CMDeviceCollection -LimitingCollectionName "All Desktop and Server Clients" -Name $Script:FirefoxOutDateColl
-    Add-CMDeviceCollectionQueryMembershipRule -CollectionName $Script:FirefoxOutDateColl -RuleName "Firefox Outdated" -QueryExpression $query -Force
-    New-CMDeviceCollection -LimitingCollectionName $Script:FirefoxOutDateColl -Name $Script:FirefoxColl
-
-    #Build updated Detection Method Clauses
-    $clause1 = New-CMDetectionClauseFile -Value -Path "%ProgramFiles%\Mozilla Firefox\" -FileName "Firefox.exe" -PropertyType Version -ExpressionOperator GreaterEquals -ExpectedValue $Script:FirefoxVersion
-    $clause2 = New-CMDetectionClauseFile -Value -Path "%ProgramFiles(x86)%\Mozilla Firefox\" -FileName "firefox.exe" -PropertyType Version -ExpressionOperator GreaterEquals -ExpectedValue $Script:FirefoxVersion 
-    $clause1.Connector = 'Or'
-    $clause2.Connector = 'Or'
-    #pulling Package information to update
-    $SDMPackageXML = (Get-CMDeploymentType -ApplicationName "$($Script:CurrentPkgName)" -DeploymentTypeName "$($Script:CurrentPkgName)").SDMPackageXML
-    [string[]]$OldDetections = (([regex]'(?<=SettingLogicalName=.)([^"]|\\")*').Matches($SDMPackageXML)).Value
-    #paramaters set, populatating chagnes in SCCM now
-    Write-Host "Updating Deployment Packages" -ForegroundColor Yellow
-    Set-CMApplication -Name $Script:CurrentPkgName -SoftwareVersion $Script:FirefoxVersion
-    Set-CMScriptDeploymentType -ApplicationName $Script:CurrentPkgName -DeploymentTypeName $Script:CurrentPkgName -ContentLocation $Script:FinalDirectory\$Script:FirefoxVersion -RemoveDetectionClause $OldDetections -AddDetectionClause($clause1,$clause2)
-    #Set-CMScriptDeploymentType -ApplicationName $PreviousPkgName -DeploymentTypeName $PreviousPkgName -ContentLocation $PreviousLocation
-    Write-Host "Redistributing Content" -ForegroundColor Yellow
-    Update-CMDistributionPoint -ApplicationName $Script:CurrentPkgName -DeploymentTypeName $Script:CurrentPkgName
-    Set-CMApplicationDeployment -ApplicationName $Script:CurrentPkgName -CollectionName $Script:FirefoxColl -AvailableDateTime (get-date 06:00:00).AddDays(0) -DeadlineDateTime (get-date 18:00:00).AddDays(1)
-    #return to local disk
-    sl c:        
-}
 
 GetVersion
 GetFirefox
 CreateBatch
 UpdateFiles
-#If ($FirstRun){FirstRun}
-#Else {
-    SetSCCMDeployment
-#} 
+If ($FirstRun){SetSCCMDeployment -FirstRun} Else {SetSCCMDeployment} 
 Write-Host "Complete!" -ForegroundColor Yellow
